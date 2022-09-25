c2348830eeaa0a22faa6493b98da6f3b05d37bfb
DanteASC4 <65970988+Zexov@users.noreply.github.com>
2022-09-25 11:26:54 -0400

    Favicon, navbar, basic styling
    - Added favicon
    - Added base navbar
    - Background now set to black
    - Added a couple custom colors to tailwind config

--- src/HyperDemonInfo.Client/wwwroot/tailwind.config.js
+++ src/HyperDemonInfo.Client/wwwroot/tailwind.config.js
@@ -1,5 +1,27 @@
 module.exports = {
-	content: [
-		'./**/*.{html,razor}',
-	]
+	content: ["./**/*.{html,razor}"],
+	darkMode: "media",
+	theme: {
+		extend: {
+			textColor: {
+				primary: "#cab158",
+				secondary: "#4c5590",
+			},
+			colors: {
+				black: {
+					0: "#000",
+					DEFAULT: "#121212",
+					100: "#1D1D1D",
+					200: "#212121",
+					300: "#242424",
+					400: "#272727",
+					500: "#2C2C2C",
+					600: "#2D2D2D",
+					700: "#323232",
+					800: "#313131",
+					900: "#373737",
+				},
+			},
+		},
+	},
 };
